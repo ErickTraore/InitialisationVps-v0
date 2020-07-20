a0be341640713f7623b85a6b8c2f44b920b32d33
root <root@vps71804.serveur-vps.net>
2020-07-20 21:59:50 +0200

    version2

--- src/Controller/PageAccueilController.php
+++ src/Controller/PageAccueilController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageAccueilController extends AbstractController
 {
     /**
-     * @Route("/", name="page_accueil, {get}")
+     * @Route("/", name="accueil")
      */
     public function index()
     {
